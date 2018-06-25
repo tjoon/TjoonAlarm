000e57d67b4c7db31adfae89891b7e588b4c7e39
tjoon <bbjoejoe21@gmail.com>
2018-06-26 04:34:24 +0900

    ADD make big frame viewpager, fragment_alarm, fragment_timer

---
+++ Android/app/src/main/java/comtjoon/github/tjoonalarm/FragmentAlarm.kt
@@ -0,0 +1,15 @@
+package comtjoon.github.tjoonalarm
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentAlarm : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.fragment_alarm,container,false)
+
+        return view
+    }
+}
--- Android/app/src/main/java/comtjoon/github/tjoonalarm/MainActivity.kt
+++ Android/app/src/main/java/comtjoon/github/tjoonalarm/MainActivity.kt
@@ -2,11 +2,16 @@ package comtjoon.github.tjoonalarm
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +19,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        tjoon_viewPager.adapter=TjoonViewPagerAdapter(supportFragmentManager)
+        tabLayout.addTab(tabLayout.newTab().setText("알람"))
+        tabLayout.addTab(tabLayout.newTab().setText("타이머"))
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tjoon_viewPager.setCurrentItem(tab!!.position)
+            }
+
+        })
+
+        tjoon_viewPager.addOnPageChangeListener(object:TabLayout.TabLayoutOnPageChangeListener(tabLayout){})
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    class TjoonViewPagerAdapter(fm:FragmentManager):FragmentStatePagerAdapter(fm){
+
+        var fragments=arrayOf(FragmentAlarm(), FragmentTimer())
+
+        // 화면을 넣어주는 부분
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        // 페이지 개수를 새는 부분
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
